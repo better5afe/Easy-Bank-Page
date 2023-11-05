09c0bd28c6c7db0259733ba78312838f30ccdfc6
better5afe <abramczyk2704@gmail.com>
2023-11-05 20:28:31 +0100

    Implemented Header components and styled them for smaller viewports

--- src/components/UI/Header.tsx
+++ src/components/UI/Header.tsx
@@ -1,9 +1,13 @@
 import Wrapper from '../reusable/Wrapper';
 import HeaderDetails from '../subcomponents/header-sub/HeaderDetails';
+import HeaderHero from '../subcomponents/header-sub/HeaderHero';
+
+import './Header.scss';
 
 const Header = () => {
 	return (
 		<header className='header'>
+				<HeaderHero />
 			<Wrapper className='header__wrapper'>
 				<HeaderDetails />
 			</Wrapper>
