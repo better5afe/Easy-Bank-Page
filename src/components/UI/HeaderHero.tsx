762c14a98f105e8fa4925a8a5f4472555f2d9f7d
better5afe <abramczyk2704@gmail.com>
2023-11-06 14:27:04 +0100

    Added header images

--- src/components/UI/HeaderHero.tsx
+++ src/components/UI/HeaderHero.tsx
@@ -1,6 +1,7 @@
 import HeroMobileBg from '../../assets/images/header/HeroMobileBg';
 import HeroDesktopBgLeft from '../../assets/images/header/HeroDesktopBgLeft';
 import HeroDesktopBgRight from '../../assets/images/header/HeroDesktopBgRight';
+import HeroImg from '../../assets/images/header/image-mockups.png';
 
 import './HeaderHero.scss';
 
@@ -11,6 +12,13 @@ const HeaderHero = () => {
 				<HeroMobileBg className='header__hero-bg-icon header__hero-bg-icon--mobile' />
 				<HeroDesktopBgLeft className='header__hero-bg-icon header__hero-bg-icon--desktop header__hero-bg-icon--desktop--left' />
 				<HeroDesktopBgRight className='header__hero-bg-icon header__hero-bg-icon--desktop header__hero-bg-icon--desktop--right' />
+			</div>
+			<div className='header__hero-carousel'>
+				<img
+					src={HeroImg}
+					alt='Four mobile phones showcasing the bank application'
+					className='header__hero-carousel-img'
+				/>
 			</div>
 		</div>
 	);
